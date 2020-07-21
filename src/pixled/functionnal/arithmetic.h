e8c4abb297c3bcd11a9925027cd80e7094736781
Paul Breugnot <paul.breugnot@univ-fcomte.fr>
2020-07-21 17:23:58 +0200

    Modulus operator

--- src/pixled/functionnal/arithmetic.h
+++ src/pixled/functionnal/arithmetic.h
@@ -257,5 +257,68 @@ namespace pixled {
 				return {std::forward<Arg1>(f1), c2};
 			}
 
+	template<typename R, typename P1, typename P2>
+		class Modulus : public api::BinaryFunction<R, P1, P2, Modulus<R, P1, P2>> {
+			public:
+				using api::BinaryFunction<R, P1, P2, Modulus<R, P1, P2>>::BinaryFunction;
+
+				R operator()(api::Point c, Time t) const override {
+					return (*this->f1)(c, t) % (*this->f2)(c, t);
+				}
+		};
+
+	template<typename Arg1, typename Arg2,
+		typename Enable = typename std::enable_if<
+			!std::is_arithmetic<typename std::remove_reference<Arg1>::type>::value
+				&& !std::is_arithmetic<typename std::remove_reference<Arg2>::type>::value>::type>
+			Modulus<
+			// R
+			typename std::common_type<
+				typename std::remove_reference<Arg1>::type::Type,
+				typename std::remove_reference<Arg2>::type::Type
+			>::type,
+			// P1
+			typename std::remove_reference<Arg1>::type::Type,
+			// P2
+			typename std::remove_reference<Arg2>::type::Type
+			> operator%(Arg1&& f1, Arg2&& f2) {
+				return {std::forward<Arg1>(f1), std::forward<Arg2>(f2)};
+			}
+
+	template<typename Arg1, typename Arg2,
+		typename Enable = typename std::enable_if<
+			std::is_arithmetic<typename std::remove_reference<Arg1>::type>::value
+				&& !std::is_arithmetic<typename std::remove_reference<Arg2>::type>::value>::type>
+			Modulus<
+			// R
+			typename std::common_type<
+				typename std::remove_reference<Arg1>::type,
+				typename std::remove_reference<Arg2>::type::Type
+			>::type,
+			// P1
+			typename std::remove_reference<Arg1>::type,
+			// P2
+			typename std::remove_reference<Arg2>::type::Type
+			> operator%(Arg1&& c1, Arg2&& f2) {
+				return {c1, std::forward<Arg2>(f2)};
+			}
+
+	template<typename Arg1, typename Arg2,
+		typename Enable = typename std::enable_if<
+			!std::is_arithmetic<typename std::remove_reference<Arg1>::type>::value
+				&& std::is_arithmetic<typename std::remove_reference<Arg2>::type>::value>::type>
+			Modulus<
+			// R
+			typename std::common_type<
+				typename std::remove_reference<Arg1>::type::Type,
+				typename std::remove_reference<Arg2>::type
+			>::type,
+			// P1
+			typename std::remove_reference<Arg1>::type::Type,
+			// P2
+			typename std::remove_reference<Arg2>::type
+			> operator%(Arg1&& f1, Arg2&& c2) {
+				return {std::forward<Arg1>(f1), c2};
+			}
 }
 #endif
--- tests/src/pixled/functionnal/arithmetic.cpp
+++ tests/src/pixled/functionnal/arithmetic.cpp
@@ -69,3 +69,32 @@ TEST_F(MinusOperator, simple_minus) {
 
 	ASSERT_EQ(minus(c, t), -14);
 }
+
+class ModulusOperator : public OperatorTest {};
+
+TEST_F(ModulusOperator, test) {
+	MockFunction<long> f1;
+	MockFunction<long>* f1_copy_1 {new MockFunction<long>};
+	MockFunction<long>* f1_copy_1_copy {new MockFunction<long>};
+	MockFunction<long>* f1_copy_2 {new MockFunction<long>};
+	MockFunction<long> f2;
+	MockFunction<long>* f2_copy {new MockFunction<long>};
+	MockFunction<long>* f2_copy_copy {new MockFunction<long>};
+
+    EXPECT_CALL(f1, copy)
+		.WillOnce(Return(f1_copy_1))
+		.WillOnce(Return(f1_copy_2));
+    EXPECT_CALL(*f1_copy_1, copy).WillOnce(Return(f1_copy_1_copy));
+    EXPECT_CALL(f2, copy).WillOnce(Return(f2_copy));
+    EXPECT_CALL(*f2_copy, copy).WillOnce(Return(f2_copy_copy));
+
+	auto modulus_1 = f1 % f2;
+
+	auto modulus = modulus_1 % f1;
+
+	EXPECT_CALL(*f1_copy_1_copy, call(c, t)).WillRepeatedly(Return(24));
+	EXPECT_CALL(*f1_copy_2, call(c, t)).WillRepeatedly(Return(24));
+	EXPECT_CALL(*f2_copy_copy, call(c, t)).WillRepeatedly(Return(10));
+
+	ASSERT_EQ(modulus(c, t), (24 % 10) % 24);
+}
