405120436bfde0e4eb02627319ef1ef859fc20c8
Paul Breugnot <paul.breugnot@univ-fcomte.fr>
2020-07-21 17:06:01 +0200

    Minus operator

--- tests/src/pixled/functionnal/arithmetic.cpp
+++ tests/src/pixled/functionnal/arithmetic.cpp
@@ -41,3 +41,31 @@ TEST_F(PlusOperator, simple_plus) {
 	ASSERT_EQ(plus(c, t), 34);
 }
 
+class MinusOperator : public OperatorTest {};
+
+TEST_F(MinusOperator, simple_minus) {
+	MockFunction<float> f1;
+	MockFunction<float>* f1_copy_1 {new MockFunction<float>};
+	MockFunction<float>* f1_copy_1_copy {new MockFunction<float>};
+	MockFunction<float>* f1_copy_2 {new MockFunction<float>};
+	MockFunction<float> f2;
+	MockFunction<float>* f2_copy {new MockFunction<float>};
+	MockFunction<float>* f2_copy_copy {new MockFunction<float>};
+
+    EXPECT_CALL(f1, copy)
+		.WillOnce(Return(f1_copy_1))
+		.WillOnce(Return(f1_copy_2));
+    EXPECT_CALL(*f1_copy_1, copy).WillOnce(Return(f1_copy_1_copy));
+    EXPECT_CALL(f2, copy).WillOnce(Return(f2_copy));
+    EXPECT_CALL(*f2_copy, copy).WillOnce(Return(f2_copy_copy));
+
+	auto minus_1 = f1 - f2;
+
+	auto minus = minus_1 - f1;
+
+	EXPECT_CALL(*f1_copy_1_copy, call(c, t)).WillRepeatedly(Return(10));
+	EXPECT_CALL(*f1_copy_2, call(c, t)).WillRepeatedly(Return(10));
+	EXPECT_CALL(*f2_copy_copy, call(c, t)).WillRepeatedly(Return(14));
+
+	ASSERT_EQ(minus(c, t), -14);
+}
